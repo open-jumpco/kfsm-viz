aa9f04fd1d4b2a2affcbcd3ba20ad3cb6b5b9816
corneil_jumpco <corneil@jumpco.io>
2021-07-02 12:40:19 +0200

    update license

--- src/test/kotlin/PacketReader.kt
+++ src/test/kotlin/PacketReader.kt
@@ -1,16 +1,14 @@
-import CharacterConstants.Companion
-import ReaderEvents.BYTE
-import ReaderEvents.CTRL
-import ReaderEvents.ESC
-import ReaderStates.CHKSUM
-import ReaderStates.END
-import ReaderStates.RCVCHK
-import ReaderStates.RCVCHKESC
-import ReaderStates.RCVDATA
-import ReaderStates.RCVESC
-import ReaderStates.RCVPCKT
-import ReaderStates.START
-import CharacterConstants.*
+/*
+ * Copyright (c) 2020-2021. Open JumpCO
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+import ReaderEvents.*
+import ReaderStates.*
 import io.jumpco.open.kfsm.stateMachine
 import java.io.ByteArrayOutputStream
 
@@ -227,7 +225,7 @@ class PacketReaderFSM(private val packetHandler: PacketHandler) {
             CharacterConstants.STX,
             CharacterConstants.ACK,
             CharacterConstants.NAK -> fsm.sendEvent(CTRL, byte)
-            else                   -> fsm.sendEvent(BYTE, byte)
+            else -> fsm.sendEvent(BYTE, byte)
         }
     }
 }
